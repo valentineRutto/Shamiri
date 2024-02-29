8af9eb4499cd37cc37386635476ce5fb99e10014
valentinerutto <rutto28@gmail.com>
2024-02-29 13:00:03 +0300

    refactor retrofit setup

--- app/src/main/java/com/valentinerutto/shamiri/di/AppModule.kt
+++ app/src/main/java/com/valentinerutto/shamiri/di/AppModule.kt
@@ -1,66 +1,39 @@
 package com.valentinerutto.shamiri.di
 
-import androidx.lifecycle.ViewModel
-import com.google.gson.GsonBuilder
 import com.valentinerutto.shamiri.data.LocationDatabase
 import com.valentinerutto.shamiri.data.LocationRepository
 import com.valentinerutto.shamiri.data.remote.ApiService
+import com.valentinerutto.shamiri.data.remote.RetrofitClient.createOkClient
+import com.valentinerutto.shamiri.data.remote.RetrofitClient.createRetrofit
 import com.valentinerutto.shamiri.ui.LocationViewmodel
 import com.valentinerutto.shamiri.utils.Constants
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import org.koin.android.BuildConfig
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.core.module.Module
 import org.koin.core.scope.Scope
 import org.koin.dsl.module
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
-import java.util.concurrent.TimeUnit
 
 val networkingModule = module {
-    single<ApiService> { get<Retrofit>().create() }
+    single<ApiService> { (get() as Retrofit).create(ApiService::class.java) }
+    single { createOkClient() }
 
     single {
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.level = when (BuildConfig.BUILD_TYPE) {
-            "release" -> HttpLoggingInterceptor.Level.NONE
-            else -> HttpLoggingInterceptor.Level.BODY
-        }
-
-        OkHttpClient.Builder()
-            .addInterceptor(interceptor)
-            .connectTimeout(1, TimeUnit.MINUTES)
-            .readTimeout(1, TimeUnit.MINUTES)
-            .build()
+        createRetrofit(
+            baseUrl = Constants.BASE_URL, get()
+        )
     }
-
-    single {
-        val gson = GsonBuilder()
-            .serializeNulls()
-            .create()
-
-        Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .client(get())
-            .build()
-    }
-
 
 }
 val repositoryModule = module {
-    single { LocationRepository(get(),database().locationDao(),database().residentsDao()) }
+    single { LocationRepository(get(), database().locationDao(), database().residentsDao()) }
 }
-val viewmodelModule = module{
+val viewmodelModule = module {
 
-    viewModel {  LocationViewmodel(get())}
+    viewModel { LocationViewmodel(get()) }
 
 }
 
-val databaseModule=module{
+val databaseModule = module {
 
     single { LocationDatabase.getDatabase(context = androidContext()) }
 
